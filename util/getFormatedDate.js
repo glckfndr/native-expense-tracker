53279429ceb46bd38377c66f1accdbed5de89b23
glckfndr <obulan65@gmail.com>
2024-09-04 23:15:32 +0300

    feat: add date formatter

---
+++ util/getFormatedDate.js
@@ -0,0 +1,5 @@
+export default function getFormatedDate(date) {
+  return `${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getFullYear()}`;
+}
